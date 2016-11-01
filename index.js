3da5860896b324777b89245a27231156490ba7bd
Kasper Jacobsen <k@mackwerk.dk>
2016-11-01 13:18:27 +0100

    Minor fixes (#1)
    * Tiny fixes
    
    * Edit by @freeall

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 'use strict';
 
-const events = require('events');
-const express = require('express');
-const bodyParser = require('body-parser');
+var events = require('events');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 module.exports = function (options) {
-  const that = new events.EventEmitter();
-  const app = express();
+  options = options || {};
+  options.port = options.port || 3000;
+
+  if (!options.verificationToken) {
+    throw new Error('verificationToken not found in options');
+  }
+  if (!options.eventUrl) {
+    throw new Error('eventUrl not found in options');
+  }
+
+  var that = new events.EventEmitter();
+  var app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: false}));
 
-  app.post(`${options.eventUrl || '/slack_event'}`, function (req, res) {
+  app.post(options.eventUrl, function (req, res) {
     if (!req.body) return res.error(400, 'bad request homes');
 
-    const body = req.body.payload ? JSON.parse(req.body.payload) : req.body;
+    var body = req.body.payload ? JSON.parse(req.body.payload) : req.body;
 
-    if (!options.verificationToken || body.token !== options.verificationToken) return res.error(403, 'could not verify slack token');
+    if (body.token !== options.verificationToken) {
+      return res.error(403, 'could not verify slack token');
+    }
 
     if (body.type === 'url_verification') return res.send(body.challenge);
     if (body.type === 'event_callback') {
